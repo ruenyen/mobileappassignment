85ac270a633b9c285b85b15feb2a605e0ab870fa
ruenyen <chinry-pa15@student.tarc.edu.my>
2018-01-06 22:40:40 +0800

    version 1

---
+++ LoginFragment.java
@@ -0,0 +1,24 @@
+package my.edu.tarc.care4life;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ruenyenchin on 6/1/2018.
+ */
+
+public class LoginFragment extends Fragment {
+    public LoginFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+    }
+}
---
+++ ViewProfileFragment.java
@@ -0,0 +1,23 @@
+package my.edu.tarc.care4life;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ruenyenchin on 6/1/2018.
+ */
+
+public class ViewProfileFragment extends Fragment {
+    public ViewProfileFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
