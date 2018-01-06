85e5f4aff93f84e46b5cb9a3eef6baf96b65fd62
ruenyen <chinry-pa15@student.tarc.edu.my>
2018-01-07 03:32:01 +0800

    version 2(fragment_register haven't done)

--- LoginFragment.java
+++ LoginFragment.java
@@ -3,9 +3,11 @@ package my.edu.tarc.care4life;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by ruenyenchin on 6/1/2018.
@@ -19,6 +21,18 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+        Button buttonRegister = (Button) view.findViewById(R.id.buttonRegister);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RegisterFragment rf = new RegisterFragment();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.frame_container, rf);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
         return view;
     }
 }
--- RegisterFragment.java
+++ RegisterFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by ruenyenchin on 6/1/2018.
@@ -18,6 +19,7 @@ public class RegisterFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        return view;
     }
 }
